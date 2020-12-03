6eb142aeba7370645c18137fe2bb887a3ae36917
Marcos Paulo <marcospsilva222@hotmail.com>
2020-12-03 17:05:59 -0300

    ajuste cors

--- src/main/java/br/com/marcos/clientes/cors/CorsConfiguration.java
+++ src/main/java/br/com/marcos/clientes/cors/CorsConfiguration.java
@@ -31,10 +31,14 @@ public class CorsConfiguration implements Filter {
 		response.setHeader("Access-Control-Allow-Origin", originPermitida);
         response.setHeader("Access-Control-Allow-Credentials", "true");
 		
-		if ("OPTIONS".equals(request.getMethod()) && originPermitida.equals(request.getHeader("Origin"))) {
-			response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
-        	response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
-        	response.setHeader("Access-Control-Max-Age", "3600");
+		if ("OPTIONS".equals(request.getMethod())) {
+			response.setHeader("Access-Control-Allow-Origin", "*");
+	        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, PATCH");
+	        response.setHeader("Access-Control-Max-Age", "3600");
+	        response.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+	        response.setHeader("Access-Control-Expose-Headers", "Location");
+//        	response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
+//        	response.setHeader("Access-Control-Max-Age", "3600");
 			
 			response.setStatus(HttpServletResponse.SC_OK);
 		} else {
@@ -42,6 +46,7 @@ public class CorsConfiguration implements Filter {
 		}
 		
 	}
+	
 	
 	@Override
 	public void destroy() {
